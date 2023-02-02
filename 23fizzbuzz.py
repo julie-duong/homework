0a29919366135fefbd7423ab06a2745914be36f2
julie-duong <julieduong@Julies-MacBook-Pro-3.local>
2023-02-01 23:17:54 -0800

    update

--- 23fizzbuzz.py
+++ 23fizzbuzz.py
@@ -7,6 +7,15 @@
 
 # Note: this is a common interview question
 
+for i in range(1,101):
+	if i % 3 == 0 and i % 5 == 0:
+		print('FizzBuzz')
+	elif i % 3 == 0:
+		print('Fizz')
+	elif i % 5 == 0:
+		print('Buzz')
+	else:
+		print(i)
 
 """
 python3 23fizzbuzz.py
