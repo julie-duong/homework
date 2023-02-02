428ff16e46dadae3241441782a0bff18069a2b43
julie-duong <julieduong@Julies-MacBook-Pro-3.local>
2023-02-02 00:36:58 -0800

    update

--- 25atseq.py
+++ 25atseq.py
@@ -8,6 +8,25 @@
 
 # Note: set random.seed() if you want repeatable random numbers
 
+import random 
+
+length = 30
+AT_fraction = 0
+seq = ''
+
+for i in range(length):
+	r = random.randint(1,100)
+	if r <= 60:
+		seq = seq + random.choice('AT')
+		AT_fraction += 1 
+	else:
+		seq = seq + random.choice('GC')
+		
+print(length, AT_fraction/length, seq)
+
+
+	
+
 
 """
 python3 25atseq.py
