ac271f2836f77be0e0381f093f03eef4091f98f5
julie-duong <julieduong@campus-004-043.ucdavis.edu>
2023-02-02 09:39:29 -0800

    update

--- 26anti.py
+++ 26anti.py
@@ -7,6 +7,19 @@
 
 dna = 'ACTGAAAAAAAAAAA'
 
+for i in range(len(dna), 0, -1):
+
+	if dna[i-1] == 'A':
+		print('T', end='')
+	elif dna[i-1] == 'T':
+		print('A', end='')
+	elif dna[i-1] == 'G':
+		print('C', end='')
+	else:
+		print('G', end='')
+
+
+	
 
 """
 python3 26anti.py
