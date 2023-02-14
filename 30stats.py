4d27e5cecfeddc168b50b3858257dba35201529f
julie-duong <julieduong@Julies-MacBook-Pro-3.local>
2023-02-13 21:26:13 -0800

    update

--- 30stats.py
+++ 30stats.py
@@ -7,6 +7,43 @@
 
 # Note: you are not allowed to import any library except sys
 
+import sys 
+
+#print(sys.argv)
+
+vals = []
+
+for thing in sys.argv[1:]:
+	vals.append(float(thing))
+
+vals.sort()
+
+# count
+count = len(vals)
+print("Count:", count)
+
+# minimum 
+print("Minimum:", min(vals))
+
+# maximum
+print("Maximum:", max(vals))
+
+# Mean 
+mean = sum(vals)/count
+print("Mean:", "%.3f"%mean)
+
+# Standard Deviation
+variance = 0.0
+for x in vals:
+	variance += (x-mean)**2
+std = (variance/(count))**0.5
+print("Standard Deviation:", "%.3f"%std)
+
+# Median
+median = vals[int(count/2)]
+print("Median:", "%.3f"%median)
+
+
 
 """
 python3 30stats.py 3 1 4 1 5
