f52cb91a2310da85981c92b9cb38855a056c5386
julie-duong <julieduong@Julies-MacBook-Pro-3.local>
2023-02-19 18:23:45 -0800

    update

--- 33birthday.py
+++ 33birthday.py
@@ -11,6 +11,28 @@
 # Variation: try making the birthdays a list
 
 
+import sys
+
+days = int(sys.argv[1])
+people = int(sys.argv[2])
+
+fact_days = 1
+fact_people = 1
+
+# calculate factorial number of days
+for i in range(1, days + 1):
+	fact_days = fact_days * i
+	
+# calculate factorial of number of days - number of people 
+for i in range(1, (days - people) + 1):
+	fact_people = fact_people * i
+
+bday_norep = fact_days/fact_people # birthdays without repetition
+bday_total = days**people
+prob = 1 - (bday_norep/bday_total)
+
+print("%.3f" % prob)
+
 """
 python3 33birthday.py 365 23
 0.571
